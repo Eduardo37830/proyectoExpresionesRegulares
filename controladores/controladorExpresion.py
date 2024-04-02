3fb71c306695bc87f5c0dc820febb3a048f66b2a
Eduardo Villamil <eduardo.villamil37830@ucaldas.edu.co>
2024-04-01 21:21:39 -0500

    Cambios en Expresion Regular y el controlador para los paréntesis

--- controladores/controladorExpresion.py
+++ controladores/controladorExpresion.py
@@ -1,5 +1,69 @@
-from modelos.expresionRegular import ModeloExpresionesRegulares
+from modelos.expresionRegular import Automata
 
-modelo = ModeloExpresionesRegulares()
-modelo.generar_automata("(a)*(a+b)(b)*")
-modelo.mostrar_automata()
+class ControladorExpresion:
+    @staticmethod
+    def build_automaton_from_regex(regex):
+        states = set()
+        alphabet = set()
+        transitions = {}
+        initial_state = 'q0'
+        accepting_states = set()
+
+        def add_transition(from_state, to_state, input_symbol):
+            nonlocal states, alphabet, transitions
+            states.add(from_state)
+            states.add(to_state)
+            alphabet.add(input_symbol)
+            transitions[(from_state, input_symbol)] = to_state
+
+        def parse_regex(substr, start_state):
+            nonlocal accepting_states
+
+            current_state = start_state
+            i = 0
+
+            while i < len(substr):
+                if i < len(substr) - 1 and substr[i + 1] == '*':
+                    new_state = 'q' + str(len(states) + 1)
+                    add_transition(current_state, new_state, substr[i])
+                    add_transition(new_state, new_state, '')
+                    current_state = new_state
+                    i += 1
+                elif i < len(substr) - 1 and substr[i + 1] == '+':
+                    new_state = 'q' + str(len(states) + 1)
+                    add_transition(current_state, new_state, substr[i])
+                    add_transition(new_state, new_state, substr[i])
+                    current_state = new_state
+                    i += 1
+                elif substr[i] == '?':
+                    new_state = 'q' + str(len(states) + 1)
+                    add_transition(current_state, new_state, '')
+                    current_state = new_state
+                elif substr[i] == '|':
+                    accepting_states.add(current_state)
+                    current_state = start_state
+                elif substr[i] == '(':
+                    group_end = substr.find(')', i + 1)
+                    if group_end != -1:
+                        group_substr = substr[i + 1:group_end]
+                        group_start_state = current_state
+                        parse_regex(group_substr, group_start_state)
+                        i = group_end
+                else:
+                    new_state = 'q' + str(len(states) + 1)
+                    add_transition(current_state, new_state, substr[i])
+                    current_state = new_state
+                i += 1
+
+            accepting_states.add(current_state)
+
+        parse_regex(regex, initial_state)
+
+        return Automata(states, alphabet, transitions, initial_state, accepting_states)
+
+
+# Ejemplo de uso
+regex = input("Ingrese la expresión regular: ")
+automaton = ControladorExpresion.build_automaton_from_regex(regex)
+print("Estados del autómata:", automaton.states)
+automaton.draw()
--- modelos/expresionRegular.py
+++ modelos/expresionRegular.py
@@ -1,39 +1,45 @@
-import re
+# NOTA:Este código fue tomado de exp3.py
+
+import pydot
 
 
-class ModeloExpresionesRegulares:
-    def __init__(self):
-        self.alfabeto = set()
-        self.estados = set()
-        self.estado_inicial = None
-        self.estados_aceptacion = set()
-        self.transiciones = {}
+class Automata:
+    def __init__(self, states, alphabet, transitions, initial_state, accepting_states):
+        self.states = states
+        self.alphabet = alphabet
+        self.transitions = transitions
+        self.current_state = initial_state
+        self.accepting_states = accepting_states
 
-    def generar_automata(self, expresion_regular):
-        self.alfabeto = set(re.findall(r'[a-zA-Z]', expresion_regular))  # Obtener el alfabeto de la expresión regular
-        self.estados = set()  # Los estados se generarán dinámicamente durante el proceso
-        self.estado_inicial = 'q0'
-        self.estados_aceptacion = set()
-        self.transiciones = {}
+    def transition(self, input_symbol):
+        if input_symbol in self.alphabet and (self.current_state, input_symbol) in self.transitions:
+            self.current_state = self.transitions[(self.current_state, input_symbol)]
+        else:
+            print("Invalid input or transition undefined.")
 
-        # Iniciar el proceso de conversión de expresión regular a autómata
-        # Aquí debes implementar un algoritmo para convertir la expresión regular en un autómata
-        # Puedes utilizar métodos como Thompson's Construction o convertir la expresión en un árbol de sintaxis y luego en un autómata
+    def is_accepting(self):
+        return self.current_state in self.accepting_states
 
-        # Ejemplo: creación de un autómata de ejemplo
-        self.estados.add(self.estado_inicial)
-        estado_final = 'q1'
-        self.estados.add(estado_final)
-        self.estados_aceptacion.add(estado_final)
-        self.transiciones[self.estado_inicial] = {'ε': [estado_final]}  # Transición epsilon al estado final
+    def draw(self):
+        graph = pydot.Dot(graph_type='digraph')
 
-    def mostrar_automata(self):
-        print("Alfabeto:", self.alfabeto)
-        print("Estados:", self.estados)
-        print("Estado inicial:", self.estado_inicial)
-        print("Estados de aceptación:", self.estados_aceptacion)
-        print("Transiciones:")
-        for estado, transiciones in self.transiciones.items():
-            for simbolo, estados_siguientes in transiciones.items():
-                for estado_siguiente in estados_siguientes:
-                    print(f"{estado} --({simbolo})--> {estado_siguiente}")
+        # Agregar nodos
+        for state in self.states:
+            node = pydot.Node(state)
+            if state in self.accepting_states:
+                node.set_shape('doublecircle')
+            graph.add_node(node)
+
+        # Agregar arcos
+        for (from_state, input_symbol), to_state in self.transitions.items():
+            label = input_symbol
+            if input_symbol == "":
+                label = "ε"  # Para transiciones epsilon
+            edge = pydot.Edge(from_state, to_state, label=label)
+            graph.add_edge(edge)
+
+        # Guardar y mostrar el diagrama
+        graph.write_png('automata.png', encoding='utf-8')
+        graph.write_pdf('automata.pdf', encoding='utf-8')
+
+
