bfcb08f1975e55c1207a9d40a130a8fcaac5c00c
Eduardo Villamil <eduardo.villamil37830@ucaldas.edu.co>
2024-05-10 17:33:52 -0500

    Adaptaciones para la segunda parte del proyecto: Operaciones entre autómatas

--- main.py
+++ main.py
@@ -3,57 +3,83 @@ from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk
 from controladores.controladorExpresion import ControladorExpresion
 
+
 class AutomataGUI:
     def __init__(self, master):
         self.master = master
         self.master.title('Visualizador de Autómatas')
         self.master.geometry("1920x1080")
 
-        self.label = tk.Label(master, text="Ingrese la expresión regular:")
-        self.label.pack()
+        self.label1 = tk.Label(master, text="Ingrese la expresión regular para el primer autómata:")
+        self.label1.pack()
 
-        self.entry = tk.Entry(master)
-        self.entry.pack()
+        self.entry1 = tk.Entry(master)
+        self.entry1.pack()
 
-        self.generate_button = tk.Button(
-            master, text="Generar y Mostrar Autómata", command=self.generate_automaton)
-        self.generate_button.pack()
+        self.generate_button1 = tk.Button(
+            master, text="Generar y Mostrar Primer Autómata", command=lambda: self.generate_automaton(1))
+        self.generate_button1.pack()
 
-        self.select_button = tk.Button(
-            master, text="Seleccionar Autómata", command=self.seleccionar_automata)
-        self.select_button.pack()
+        self.select_button1 = tk.Button(
+            master, text="Seleccionar Primer Autómata", command=lambda: self.seleccionar_automata(1))
+        self.select_button1.pack()
 
-        self.image_label = tk.Label(master)
-        self.image_label.pack()
+        self.image_label1 = tk.Label(master)
+        self.image_label1.pack()
+
+        self.label2 = tk.Label(master, text="Ingrese la expresión regular para el segundo autómata:")
+        self.label2.pack()
+
+        self.entry2 = tk.Entry(master)
+        self.entry2.pack()
+
+        self.generate_button2 = tk.Button(
+            master, text="Generar y Mostrar Segundo Autómata", command=lambda: self.generate_automaton(2))
+        self.generate_button2.pack()
+
+        self.select_button2 = tk.Button(
+            master, text="Seleccionar Segundo Autómata", command=lambda: self.seleccionar_automata(2))
+        self.select_button2.pack()
+
+        self.image_label2 = tk.Label(master)
+        self.image_label2.pack()
 
         self.generated_data = []  # Almacenamos tuplas de (ruta_de_imagen, expresion_regular)
         self.register_file = "automata_register.txt"
         self.read_register()
 
-    def generate_automaton(self):
-        regex = self.entry.get()
+    def generate_automaton(self, automaton_number):
+        if automaton_number == 1:
+            regex = self.entry1.get()
+        else:
+            regex = self.entry2.get()
+
         if regex:
             try:
                 automaton = ControladorExpresion.build_automaton_from_regex(regex)
                 image_path = automaton.draw()
                 self.generated_data.append((image_path, regex))  # Guardar tupla
-                self.display_image(image_path)
+                self.display_image(image_path, automaton_number)
                 self.write_register()
             except Exception as e:
                 messagebox.showerror("Error", f"Ocurrió un error al generar el autómata: {str(e)}")
         else:
             messagebox.showwarning("Advertencia", "Por favor, ingrese una expresión regular.")
 
-    def display_image(self, image_path):
+    def display_image(self, image_path, automaton_number):
         try:
             image = Image.open(image_path)
             photo = ImageTk.PhotoImage(image)
-            self.image_label.config(image=photo)
-            self.image_label.image = photo
+            if automaton_number == 1:
+                self.image_label1.config(image=photo)
+                self.image_label1.image = photo
+            else:
+                self.image_label2.config(image=photo)
+                self.image_label2.image = photo
         except Exception as e:
             messagebox.showerror("Error", f"No se pudo cargar la imagen del autómata: {str(e)}")
 
-    def seleccionar_automata(self):
+    def seleccionar_automata(self, automaton_number):
         selection_window = tk.Toplevel(self.master)
         selection_window.geometry("800x600")
         selection_window.title("Seleccione un Autómata")
@@ -67,9 +93,13 @@ class AutomataGUI:
             if listbox.curselection():
                 selected_index = listbox.curselection()[0]
                 selected_image_path, selected_regex = self.generated_data[selected_index]
-                self.display_image(selected_image_path)
-                self.entry.delete(0, tk.END)
-                self.entry.insert(0, selected_regex)
+                self.display_image(selected_image_path, automaton_number)
+                if automaton_number == 1:
+                    self.entry1.delete(0, tk.END)
+                    self.entry1.insert(0, selected_regex)
+                else:
+                    self.entry2.delete(0, tk.END)
+                    self.entry2.insert(0, selected_regex)
                 selection_window.destroy()
 
         listbox.bind('<<ListboxSelect>>', on_select)
@@ -87,6 +117,7 @@ class AutomataGUI:
         with open(self.register_file, "w") as file:
             for img_path, regex in self.generated_data:
                 file.write(f"{img_path},{regex}\n")
+
 
 if __name__ == "__main__":
     root = tk.Tk()
